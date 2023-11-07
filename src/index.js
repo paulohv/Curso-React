abe7b5b2da001b30af2cdf443ac15210f695a6f6
paulohv <paulo.herrera94@gmail.com>
2023-11-07 18:16:06 -0300

    heroes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
-const { useCallback } = require("react")
+import { heroes } from './data/heroes'
 
-const personajes = ['Goku', 'Vegeta', 'Trunks']
-const [ , , p3 ] = personajes
-console.log( p3 )
-
-const retornaArreglo = () => {
-    return ['ABC', 123]
-}
-
-const [ letras, numeros ] = retornaArreglo ()
-console.log(letras, numeros)
-
-//Tarea
-//1. El primer valor del arr se llamará nombre
-//2. se llamara setNombre
-const state = ( valor ) => {
-    return [ valor, () =>{ console.log('Hola Mundo')}]
-}
-
-const [nombre, setNombre] = state('Goku')
-console.log( nombre )
-setNombre()
+console.log(heroes)
